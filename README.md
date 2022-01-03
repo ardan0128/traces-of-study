8962c2bf3fb1e011bfdeecc8c8538a5ba9b3f671
jortier <jwon1818@daum.net>
2022-01-03 22:44:21 +0900

    update toy

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# coplit-js
+``` javascript
+/posts/all-posts
+[
+  {
+    postId: 1
+    title: 'asdasd'
+    beans: []
+    userId:
+    createdAt: yyyy-MM-dd
+  }
+]
+```
+
+/posts?title='title'
+[
+  {
+    postId: 1
+    title: 'asdasd'
+    beans: []
+    userId:
+    createdAt: yyyy-MM-dd
+  }
+]
--- toy/22_rotateMatrix/my_solutation1.js
+++ toy/22_rotateMatrix/my_solutation1.js
@@ -1,28 +1,28 @@
 const rotateMatrix = function (matrix, spin = 1) {
   // TODO: 여기에 코드를 작성합니다.
-  
+
   // 회전수를 구해준다.
   // 4번 회전할때마다 360도 회전이기 때문에 회전시키않고 그대로 리턴한다.
   let realSpin = spin % 4;
-  if(realSpin === 0){
+  if (realSpin === 0) {
     return matrix;
   }
 
   // matrix가 빈 배열이면 빈 배열을 리턴한다.
-  if(matrix.length === 0){
+  if (matrix.length === 0) {
     return matrix;
   }
 
   let tempMatrix = matrix.slice();
   let result = [];
 
-  for(let rotate = 0; rotate < realSpin; rotate++){
+  for (let rotate = 0; rotate < realSpin; rotate++) {
     let spinMatrix = new Array(tempMatrix[0].length).fill(0).map(row => new Array(tempMatrix.length).fill(0));
 
     tempMatrix.reverse();
 
-    for(let i = 0; i < tempMatrix.length; i++){
-      for(let j = 0; j < tempMatrix[i].length; j++){
+    for (let i = 0; i < tempMatrix.length; i++) {
+      for (let j = 0; j < tempMatrix[i].length; j++) {
         spinMatrix[j][i] = tempMatrix[i][j];
       }
     }
