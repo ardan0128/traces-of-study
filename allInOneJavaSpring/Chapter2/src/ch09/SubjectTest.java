6384c7b62c70d8ecc6d232b0637140d1d10a931e
jortier <jwon1818@daum.net>
2022-03-07 17:20:01 +0900

    java/spring part2-09

---
+++ allInOneJavaSpring/Chapter2/src/ch09/SubjectTest.java
@@ -0,0 +1,16 @@
+package ch09;
+
+public class SubjectTest {
+  public static void main(String[] args) {
+    Student studentLee = new Student(100, "LEE");
+    studentLee.setKoreaSubject("국어", 100);
+    studentLee.setMathSubject("수학", 99);
+
+    Student studentJor = new Student(200, "Jortier");
+    studentJor.setKoreaSubject("국어", 50);
+    studentJor.setMathSubject("수학", 70);
+
+    studentLee.showScoreInfo();
+    studentJor.showScoreInfo();
+  }
+}
