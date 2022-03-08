1ff79dc2a226479633589620351283a9306ee3fc
jortier <jwon1818@daum.net>
2022-03-08 19:44:10 +0900

    Java/Spring Part2-23
    객체 배열을 구현한 클래스 ArrayList

---
+++ allInOneJavaSpring/Chapter2/src/ch23/ArrayListTest.java
@@ -0,0 +1,21 @@
+package ch23;
+
+import java.util.ArrayList;
+
+import ch21.Book;
+
+public class ArrayListTest {
+  public static void main(String[] args) {
+    ArrayList<Book> books = new ArrayList<>();
+
+    books.add(new Book("태백산맥1", "조정래"));
+    books.add(new Book("태백산맥2", "조정래"));
+    books.add(new Book("태백산맥3", "조정래"));
+    books.add(new Book("태백산맥4", "조정래"));
+    books.add(new Book("태백산맥5", "조정래"));
+
+    for (int i = 0; i < books.size(); i++) {
+      books.get(i).showInfo();
+    }
+  }
+}
