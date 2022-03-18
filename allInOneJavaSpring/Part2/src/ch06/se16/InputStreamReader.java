34af80114532cb953c82f8ee1b66c775b9455746
jortier <jwon1818@daum.net>
2022-03-18 21:52:02 +0900

    Java/Spring Part2-06-16
    여러가지 보조 스트림 클래스들

---
+++ allInOneJavaSpring/Part2/src/ch06/se16/InputStreamReader.java
@@ -0,0 +1,19 @@
+package ch06.se16;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class InputStreamReader {
+  public static void main(String[] args) {
+    try (InputStreamReader isr = new InputStreamReader(new FileInputStream("reader.txt"))) {
+      int i;
+
+      while ((i = isr.read()) != -1) {
+        System.out.println((char) i);
+      }
+    } catch (IOException e) {
+
+    }
+  }
+}
