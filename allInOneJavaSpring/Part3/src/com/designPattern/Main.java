8417bb3d309d081b2a1bafa5aeb7eb7ed0d548fe
jortier <jwon1818@daum.net>
2022-04-14 15:21:24 +0900

    Singleton Pattern

---
+++ allInOneJavaSpring/Part3/src/com/designPattern/Main.java
@@ -0,0 +1,19 @@
+package com.designPattern;
+
+import com.designPattern.singleton.AClazz;
+import com.designPattern.singleton.BClazz;
+import com.designPattern.singleton.SocketClient;
+
+public class Main {
+
+    public static void main(String[] args) {
+        AClazz aClazz = new AClazz();
+        BClazz bClazz = new BClazz();
+
+        SocketClient aClient = aClazz.getSocketClient();
+        SocketClient bClient = bClazz.getSocketClient();
+
+        System.out.println("두 개의 객체가 동일한가?");
+        System.out.println(aClient.equals(bClient));
+    }
+}
