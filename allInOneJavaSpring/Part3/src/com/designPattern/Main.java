7f206cee673e063f61dcf0cf30b981cb3b7d5aab
jortier <jwon1818@daum.net>
2022-04-14 15:43:05 +0900

    Adapter Pattern

--- allInOneJavaSpring/Part3/src/com/designPattern/Main.java
+++ allInOneJavaSpring/Part3/src/com/designPattern/Main.java
@@ -1,5 +1,6 @@
 package com.designPattern;
 
+import com.designPattern.adapter.*;
 import com.designPattern.singleton.AClazz;
 import com.designPattern.singleton.BClazz;
 import com.designPattern.singleton.SocketClient;
@@ -7,6 +8,8 @@ import com.designPattern.singleton.SocketClient;
 public class Main {
 
     public static void main(String[] args) {
+        // Singleton Pattern
+        System.out.println("Singleton Pattern");
         AClazz aClazz = new AClazz();
         BClazz bClazz = new BClazz();
 
@@ -15,5 +18,27 @@ public class Main {
 
         System.out.println("두 개의 객체가 동일한가?");
         System.out.println(aClient.equals(bClient));
+        System.out.println();
+
+        // Adapter Pattern
+        System.out.println("Adapter Pattern");
+        HairDryer hairDryer = new HairDryer();
+        connect(hairDryer);
+
+        Cleaner cleaner = new Cleaner();
+        Electronic110V cleanerAdapter = new SocketAdapter(cleaner);
+        connect(cleanerAdapter);
+
+        AirConditioner airConditioner = new AirConditioner();
+        Electronic110V airAdapter = new SocketAdapter(airConditioner);
+        connect(airAdapter);
+
+        /**
+         * Electronic110V를 계속 생성하지말고 Singleton으로 사용할 수 있지 않을까
+         */
+    }
+
+    public static void connect(Electronic110V electronic110V){
+        electronic110V.powerOn();
     }
 }
---
+++ allInOneJavaSpring/Part3/src/com/designPattern/adapter/Electronic220V.java
@@ -0,0 +1,5 @@
+package com.designPattern.adapter;
+
+public interface Electronic220V {
+    void connect();
+}
