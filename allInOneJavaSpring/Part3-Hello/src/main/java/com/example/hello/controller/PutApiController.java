ebf28ae0dbf2d3ca8a8dee2170b5fea38223c50d
jortier <jwon1818@daum.net>
2022-04-16 23:37:56 +0900

    PUT API

--- allInOneJavaSpring/Part3-Hello/src/main/java/com/example/hello/controller/PutApiController.java
+++ allInOneJavaSpring/Part3-Hello/src/main/java/com/example/hello/controller/PutApiController.java
@@ -1,10 +1,7 @@
 package com.example.hello.controller;
 
 import com.example.hello.dto.PutRequestDto;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
@@ -13,6 +10,12 @@ public class PutApiController {
     @PutMapping("/put")
     public PutRequestDto put(@RequestBody PutRequestDto putRequestDto){
         System.out.println(putRequestDto);
+        return putRequestDto;
+    }
+
+    @PutMapping("/put/{userId}")
+    public PutRequestDto put(@RequestBody PutRequestDto putRequestDto, @PathVariable Long userId){
+        System.out.println(userId);
         return putRequestDto;
     }
 }
