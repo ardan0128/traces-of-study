8dfcaf433283064eec8043c34a87de79121e18fe
jortier <jwon1818@daum.net>
2022-05-11 23:06:16 +0900

    server to server (3)

--- allInOneJavaSpring/Part3/client/src/main/java/com/example/client/controller/ApiController.java
+++ allInOneJavaSpring/Part3/client/src/main/java/com/example/client/controller/ApiController.java
@@ -1,5 +1,6 @@
 package com.example.client.controller;
 
+import com.example.client.dto.Req;
 import com.example.client.dto.UserResponse;
 import com.example.client.service.RestTemplateService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,5 +26,15 @@ public class ApiController {
     @GetMapping("/post")
     public UserResponse getPost(){
         return restTemplateService.post();
+    }
+
+    @GetMapping("/exchange")
+    public UserResponse getExchange(){
+        return restTemplateService.exchange();
+    }
+
+    @GetMapping("/generic-exchange")
+    public Req<UserResponse> genericExchange(){
+        return restTemplateService.genericExchange();
     }
 }
--- allInOneJavaSpring/Part3/client/src/main/java/com/example/client/service/RestTemplateService.java
+++ allInOneJavaSpring/Part3/client/src/main/java/com/example/client/service/RestTemplateService.java
@@ -1,7 +1,11 @@
 package com.example.client.service;
 
+import com.example.client.dto.Req;
 import com.example.client.dto.UserRequest;
 import com.example.client.dto.UserResponse;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -55,6 +59,68 @@ public class RestTemplateService {
         System.out.println(response.getStatusCode());
         System.out.println(response.getHeaders());
         System.out.println(response.getBody());
+
+        return response.getBody();
+    }
+
+    public UserResponse exchange(){
+        URI uri = UriComponentsBuilder
+                .fromUriString("http://localhost:9090")
+                .path("/api/server/user/{userId}/name/{userName}")
+                .encode()
+                .build()
+                .expand(100, "jortier")
+                .toUri();
+
+        System.out.println(uri);
+
+        UserRequest req = new UserRequest();
+        req.setName("jortier");
+        req.setAge(20);
+
+        RequestEntity<UserRequest> requestEntity = RequestEntity
+                .post(uri)
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("x-authorization", "abcd")
+                .header("custom-header", "fffff")
+                .body(req);
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        ResponseEntity<UserResponse> response = restTemplate.exchange(requestEntity, UserResponse.class);
+
+        return response.getBody();
+    }
+
+    public Req<UserResponse> genericExchange(){
+        URI uri = UriComponentsBuilder
+                .fromUriString("http://localhost:9090")
+                .path("/api/server/user1/{userId}/name/{userName}")
+                .encode()
+                .build()
+                .expand(100, "jortier")
+                .toUri();
+
+        System.out.println(uri);
+
+        UserRequest userRequest = new UserRequest();
+        userRequest.setName("jortier");
+        userRequest.setAge(20);
+
+        Req<UserRequest> req = new Req<UserRequest>();
+        req.setHeader(new Req.Header());
+        req.setResBody(userRequest);
+
+        RequestEntity<Req<UserRequest>> requestEntity = RequestEntity
+                .post(uri)
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("x-authorization", "abcd")
+                .header("custom-header", "fffff")
+                .body(req);
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        ResponseEntity<Req<UserResponse>> response = restTemplate.exchange(requestEntity, new ParameterizedTypeReference<Req<UserResponse>>() {});
 
         return response.getBody();
     }
