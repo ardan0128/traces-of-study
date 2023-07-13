2d6e26d35934d789ee8399d85288a373a67c5d44
jortier <jwon1818@daum.net>
2023-07-13 20:56:32 +0900

    [ING] BAEKJOON 1157

---
+++ algorithm/baekjoon/src/problem1157/Main.java
@@ -0,0 +1,38 @@
+package problem1157;
+
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+    String inputText = sc.next();
+
+    int[] inputCount = new int[26];
+    for(int i = 0; i < inputText.length(); i++){
+      if(inputText.charAt(i) > 'Z'){
+        System.out.println(inputText.charAt(i) - 'a');
+        inputCount[inputText.charAt(i) - 'a']++;
+      }else{
+        System.out.println(inputText.charAt(i) - 'A');
+        inputCount[inputText.charAt(i) - 'A']++;
+      }
+    }
+
+    int maxCount = 1;
+    int maxIndex = 0;
+    for(int i = 1; i < inputCount.length; i++){
+      if(inputCount[i] > inputCount[maxIndex]){
+        maxIndex = i;
+        maxCount = 1;
+      }else if(inputCount[i] == inputCount[maxIndex]){
+        maxCount++;
+      }
+    }
+
+    if(maxCount > 1){
+      System.out.println("?");
+    }else{
+      System.out.println((char) maxIndex);
+    }
+  }
+}
