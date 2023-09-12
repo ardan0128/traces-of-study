80f09a3563a3858f1889ee60744c39a17696222c
jortier <jwon1818@daum.net>
2023-09-12 17:07:06 +0900

    [ADD] BOJ 10870 - 피보나치 수 5

---
+++ algorithm/baekjoon/src/problem10870/Main.java
@@ -0,0 +1,22 @@
+package problem10870;
+
+import java.util.Scanner;
+
+public class Main {
+  static int n;
+  static long[] dy;
+
+  public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);
+    n = scanner.nextInt();
+    dy = new long[20 + 1];
+    dy[0] = 0;
+    dy[1] = 1;
+
+    for(int i = 2; i <= 20; i++){
+      dy[i] = dy[i - 2] + dy[i - 1];
+    }
+
+    System.out.println(dy[n]);
+  }
+}
