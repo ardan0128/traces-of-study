ad3777588cc1247fcd4be11327e10be2a41fa708
jortier <jwon1818@daum.net>
2023-09-16 22:25:37 +0900

    [ADD] BOJ 20364 - 부동산 다툼

---
+++ algorithm/baekjoon/src/problem20364/Main.java
@@ -0,0 +1,36 @@
+package problem20364;
+
+import java.util.Scanner;
+
+public class Main {
+  static int N, Q;
+  static boolean[] estate;
+
+  public static void main(String[] args){
+    StringBuilder sb = new StringBuilder();
+    Scanner scanner = new Scanner(System.in);
+
+    N = scanner.nextInt();
+    Q = scanner.nextInt();
+    estate = new boolean[N + 1];
+
+    while(Q-- > 0){
+      int x = scanner.nextInt();
+      int result = 0;
+      int y = x;
+
+      while(x > 0){
+        if(estate[x]){
+          result = x;
+        }
+
+        x >>= 1;
+      }
+
+      estate[y] = true;
+      sb.append(result).append("\n");
+    }
+
+    System.out.println(sb);
+  }
+}
