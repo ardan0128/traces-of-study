88b29bc69192bdaecd32ab0365202eb8dfd6d017
ardan0128 <jwon1818@daum.net>
2023-10-09 22:11:21 +0900

    [ADD] Test5_function

---
+++ jpa-study/src/test/java/com/example/jpastudy/Test6_function.java
@@ -0,0 +1,68 @@
+package com.example.jpastudy;
+
+import com.example.jpastudy.entity.Member;
+import com.example.jpastudy.entity.Team;
+import com.querydsl.core.Tuple;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static com.example.jpastudy.entity.QMember.member;
+import static com.example.jpastudy.entity.QTeam.team;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+public class Test6_function {
+  @Autowired
+  EntityManager em;
+  JPAQueryFactory query;
+
+  @BeforeEach
+  public void before(){
+    query = new JPAQueryFactory(em);
+    Team teamA = new Team("teamA");
+    Team teamB = new Team("teamB");
+    Member m1 = new Member("m1", 10, teamA);
+    Member m2 = new Member("m2", 20, teamA);
+    Member m3 = new Member("m3", 30, teamB);
+    Member m4 = new Member("m4", 40, teamB);
+
+    em.persist(teamA);
+    em.persist(teamB);
+    em.persist(m1);
+    em.persist(m2);
+    em.persist(m3);
+    em.persist(m4);
+  }
+
+  @Test
+  public void aggregation(){
+    List<Tuple> result = query.select(member.count(), member.age.sum(), member.age.avg(), member.age.max(), member.age.min()).from(member).fetch();
+    Tuple tuple = result.get(0);
+
+    assertThat(tuple.get(member.count())).isEqualTo(4);
+    assertThat(tuple.get(member.age.sum())).isEqualTo(100);
+    assertThat(tuple.get(member.age.avg())).isEqualTo(25);
+    assertThat(tuple.get(member.age.max())).isEqualTo(40);
+    assertThat(tuple.get(member.age.min())).isEqualTo(10);
+  }
+
+  @Test
+  public void groupBy(){
+    List<Tuple> result = query.select(team.name, member.age.avg()).from(member).join(member.team, team).groupBy(team.name).fetch();
+    Tuple teamA = result.get(0);
+    Tuple teamB = result.get(1);
+
+    assertThat(teamA.get(team.name)).isEqualTo("teamA");
+    assertThat(teamA.get(member.age.avg())).isEqualTo(15);
+    assertThat(teamB.get(team.name)).isEqualTo("teamB");
+    assertThat(teamB.get(member.age.avg())).isEqualTo(35);
+  }
+}
