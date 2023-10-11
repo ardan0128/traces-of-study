64b36720ff290a8b5f85e303557623a8d4ed7d1e
ardan0128 <jwon1818@daum.net>
2023-10-11 10:02:16 +0900

    [ADD] JPA - constant

---
+++ jpa-study/src/test/java/com/example/jpastudy/JPA_constant.java
@@ -0,0 +1,55 @@
+package com.example.jpastudy;
+
+import com.example.jpastudy.entity.Member;
+import com.example.jpastudy.entity.Team;
+import com.querydsl.core.Tuple;
+import com.querydsl.core.types.dsl.Expressions;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static com.example.jpastudy.entity.QMember.member;
+
+@SpringBootTest
+@Transactional
+public class JPA_constant {
+  @Autowired
+  EntityManager em;
+  JPAQueryFactory query;
+
+  @BeforeEach
+  public void before(){
+    query = new JPAQueryFactory(em);
+    Team teamA = new Team("teamA");
+    Team teamB = new Team("teamB");
+    Member m1 = new Member("m1", 10, teamA);
+    Member m2 = new Member("m2", 20, teamA);
+    Member m3 = new Member("m3", 30, teamB);
+    Member m4 = new Member("m4", 40, teamB);
+
+    em.persist(teamA);
+    em.persist(teamB);
+    em.persist(m1);
+    em.persist(m2);
+    em.persist(m3);
+    em.persist(m4);
+  }
+
+  @Test
+  public void constant(){
+    List<Tuple> result = query
+        .select(member.name, Expressions.constant("A"))
+        .from(member)
+        .fetch();
+
+    for(Tuple tuple : result){
+      System.out.println("tuple = " + tuple);
+    }
+  }
+}
