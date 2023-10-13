ea68741ccf97798b0acaee9e3acaae6ae36f4ed4
ardan0128 <jwon1818@daum.net>
2023-10-13 17:16:32 +0900

    [ADD] BOJ 9489 - 사촌

---
+++ algorithm/baekjoon/src/util/FastReader.java
@@ -0,0 +1,52 @@
+package util;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class FastReader {
+  BufferedReader br;
+  StringTokenizer st;
+
+  public FastReader() {
+    br = new BufferedReader(new InputStreamReader(System.in));
+  }
+
+  public FastReader(String s) throws FileNotFoundException {
+    br = new BufferedReader(new FileReader(new File(s)));
+  }
+
+  String next() {
+    while (st == null || !st.hasMoreElements()) {
+      try {
+        st = new StringTokenizer(br.readLine());
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+
+    return st.nextToken();
+  }
+
+  public int nextInt() {
+    return Integer.parseInt(next());
+  }
+
+  public String nextLine() {
+    String str = "";
+    try {
+      str = br.readLine();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    return str;
+  }
+
+  public long nextLong() {
+    return Long.parseLong(next());
+  }
+
+  public double nextDouble() {
+    return Double.parseDouble(next());
+  }
+}
