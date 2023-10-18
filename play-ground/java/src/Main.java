ce5a5abdc55aa0f1d3c4cf87e60b38f95d35fa9c
ardan0128 <jwon1818@daum.net>
2023-10-18 14:45:39 +0900

    [ADD] java http request

--- play-ground/java/src/Main.java
+++ play-ground/java/src/Main.java
@@ -1,5 +1,12 @@
+import httpRequest.HttpRequest;
+
+import java.io.IOException;
+
 public class Main {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
     System.out.println("Hello world!");
+
+    HttpRequest test = new HttpRequest();
+    test.test();
   }
 }
--- play-ground/java/src/httpRequest/HttpRequest.java
+++ play-ground/java/src/httpRequest/HttpRequest.java
@@ -1,5 +1,33 @@
 package httpRequest;
 
-public class HttpRequest {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
+public class HttpRequest {
+  public void test() throws IOException {
+    URL url = new URL("https://jsonplaceholder.typicode.com/posts");
+    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+    connection.setRequestMethod("GET");
+    connection.setRequestProperty("Conten-Type", "application/json");
+
+    int responseCode = connection.getResponseCode();
+    String responseMessage = connection.getResponseMessage();
+    BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+    StringBuffer sb = new StringBuffer();
+
+    while(br.readLine() != null){
+      sb.append(br.readLine());
+    }
+
+    br.close();
+
+    System.out.println(responseCode);
+    System.out.println(responseMessage);
+    System.out.println(sb);
+  }
 }
