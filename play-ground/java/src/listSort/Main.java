9d635255c653d047d03a732e924c532e3639a135
ardan0128 <jwon1818@daum.net>
2024-05-13 17:02:18 +0900

    [ADD] weekly toggl track 2024 W18

--- play-ground/java/src/listSort/Main.java
+++ play-ground/java/src/listSort/Main.java
@@ -1,5 +1,6 @@
 package listSort;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class Main {
@@ -10,6 +11,21 @@ public class Main {
     listSort.customSort(test);
 
     for(List<String> item : test){
+      System.out.println(item);
+    }
+
+    List<List<String>> test2 = listSort.createList();
+
+    test2.sort((o1, o2) -> {
+      if(o1.get(0).equals(o2.get(0))){
+        return o2.get(1).compareToIgnoreCase(o1.get(1));
+      }else{
+        return o2.get(0).compareToIgnoreCase(o1.get(0));
+      }
+    });
+
+    System.out.println("===================");
+    for(List<String> item : test2){
       System.out.println(item);
     }
   }
