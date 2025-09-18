afdaf57eff5972106747790c970ccea9e38db5cf
ardan0128 <jwon1818@daum.net>
2025-09-18 18:48:08 +0900

    Binary Tree Postorder Traversal

---
+++ algorithm/LeetCode/Binary Tree Postorder Traversal/Solution.java
@@ -0,0 +1,36 @@
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+
+        postorderTraversal(root, result);
+
+        return result;
+    }
+
+    public void postorderTraversal(TreeNode node, List<Integer> list) {
+        if (node == null) {
+            return;
+        }
+
+        postorderTraversal(node.left, list);
+        postorderTraversal(node.right, list);
+        list.add(node.val);
+    }
+}
