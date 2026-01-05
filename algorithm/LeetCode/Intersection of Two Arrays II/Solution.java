749a34535b9b3186ecb190aacf1505727ec4947f
ardan0128 <jwon1818@daum.net>
2026-01-05 13:22:42 +0900

    Intersection of Two Arrays II

---
+++ algorithm/LeetCode/Intersection of Two Arrays II/Solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+
+        while(i < n1 && j < n2){
+            if(nums1[i] < nums2[j]) i++;
+            else if(nums1[i] > nums2[j]) j++;
+            else{
+                nums1[k++] = nums1[i++];
+                j++;
+            }
+        }
+
+        return Arrays.copyOfRange(nums1, 0, k);
+    }
+}
