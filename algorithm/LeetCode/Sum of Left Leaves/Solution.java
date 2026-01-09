f1a0272f037f57de5958cbe79ba3e2cd380b60cc
ardan0128 <jwon1818@daum.net>
2026-01-09 14:37:31 +0900

    Sum of Left Leaves

---
+++ algorithm/LeetCode/Sum of Left Leaves/Solution.java
@@ -0,0 +1,46 @@
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    public int sumOfLeftLeaves(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int sum = 0;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+
+        while (!q.isEmpty()) {
+            TreeNode curr = q.poll();
+
+            if (curr.left != null) {
+                if (curr.left.left == null && curr.left.right == null) {
+                    sum += curr.left.val;
+                }
+
+                q.offer(curr.left);
+            }
+
+            if (curr.right != null) {
+                q.offer(curr.right);
+            }
+        }
+
+        return sum;
+    }
+}
