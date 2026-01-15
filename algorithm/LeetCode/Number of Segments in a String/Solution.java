bfe04278da4c51ffa8148fd9536a035d4674936e
ardan0128 <jwon1818@daum.net>
2026-01-15 14:49:27 +0900

    Number of Segments in a String

---
+++ algorithm/LeetCode/Number of Segments in a String/Solution.java
@@ -0,0 +1,19 @@
+
+class Solution {
+
+    public int countSegments(String s) {
+        int count = 0;
+        boolean inSegment = false;
+
+        for (char c : s.toCharArray()) {
+            if (c != ' ' && !inSegment) {
+                count++;
+                inSegment = true;
+            } else if (c == ' ') {
+                inSegment = false;
+            }
+        }
+
+        return count;
+    }
+}
