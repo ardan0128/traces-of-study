21ec8d9b136184c54587ca85b5dd190785816080
ardan0128 <jwon1818@daum.net>
2026-01-16 10:55:05 +0900

    Arranging Coins

---
+++ algorithm/LeetCode/Arranging Coins/Solution.java
@@ -0,0 +1,14 @@
+
+class Solution {
+
+    public int arrangeCoins(int n) {
+        int row = 1;
+
+        while (n > 0) {
+            row++;
+            n -= row;
+        }
+
+        return row - 1;
+    }
+}
