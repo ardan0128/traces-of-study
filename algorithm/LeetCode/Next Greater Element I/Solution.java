cafbf3a01a7ca010e14bb81cdcf38b3c296260a7
ardan0128 <jwon1818@daum.net>
2026-01-20 14:43:19 +0900

    Next Greater Element I

---
+++ algorithm/LeetCode/Next Greater Element I/Solution.java
@@ -0,0 +1,24 @@
+
+class Solution {
+
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> ng = new HashMap<>();
+        Deque<Integer> st = new ArrayDeque<>();
+
+        for (int num : nums2) {
+            while (!st.isEmpty() && st.peek() < num) {
+                ng.put(st.pop(), num);
+            }
+
+            st.push(num);
+        }
+
+        int[] result = new int[nums1.length];
+
+        for (int i = 0; i < nums1.length; i++) {
+            result[i] = ng.getOrDefault(nums1[i], -1);
+        }
+
+        return result;
+    }
+}
