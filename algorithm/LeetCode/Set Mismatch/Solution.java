efccea676acff378db1e4772dedbd4494fe71687
ardan0128 <jwon1818@daum.net>
2026-02-11 19:09:06 +0900

    Set Mismatch

---
+++ algorithm/LeetCode/Set Mismatch/Solution.java
@@ -0,0 +1,31 @@
+
+class Solution {
+
+    public int[] findErrorNums(int[] nums) {
+        int n = nums.length;
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 1; i <= n; i++) {
+            map.put(i, 1);
+        }
+
+        for (int num : nums) {
+            map.put(num, map.get(num) - 1);
+        }
+
+        int duplicate = 0;
+        int missing = 0;
+
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() == -1) {
+                duplicate = entry.getKey();
+            }
+
+            if (entry.getValue() == 1) {
+                missing = entry.getKey();
+            }
+        }
+
+        return new int[]{duplicate, missing};
+    }
+}
