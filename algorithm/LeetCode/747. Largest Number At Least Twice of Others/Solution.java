938d8b24b8e733d9b776fcdb879931f34475ef3c
ardan0128 <jwon1818@daum.net>
2026-02-22 16:48:14 +0900

    LeetCode 747. Largest Number At Least Twice of Others

---
+++ algorithm/LeetCode/747. Largest Number At Least Twice of Others/Solution.java
@@ -0,0 +1,21 @@
+
+class Solution {
+
+    public int dominantIndex(int[] nums) {
+        int max = -1;
+        int index = -1;
+        int second = -1;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > max) {
+                second = max;
+                max = nums[i];
+                index = i;
+            } else if (nums[i] > second) {
+                second = nums[i];
+            }
+        }
+
+        return second * 2 <= max ? index : -1;
+    }
+}
