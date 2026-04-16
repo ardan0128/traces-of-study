d51e995d40e98f16c2ada20e4a3f740c2cd01045
ardan0128 <jwon1818@daum.net>
2026-04-16 18:08:55 +0900

    LeetCode 1281. Subtract the Product and Sum of Digits of an Integer

---
+++ algorithm/LeetCode/1281. Subtract the Product and Sum of Digit of an Integer/Solution.java
@@ -0,0 +1,17 @@
+
+class Solution {
+
+    public int subtractProductAndSum(int n) {
+        int mul = 1;
+        int sum = 0;
+
+        while (n > 0) {
+            int digit = n % 10;
+            mul *= digit;
+            sum += digit;
+            n = n / 10;
+        }
+
+        return mul - sum;
+    }
+}
