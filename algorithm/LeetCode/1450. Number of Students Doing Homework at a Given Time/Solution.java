2072456956284808abbece3f509a48578c7d6300
JWon <jwon1818@daum.net>
2026-05-11 18:16:55 +0900

    LeetCode 1450. Number of Students Doing Homework at a Given Time

---
+++ algorithm/LeetCode/1450. Number of Students Doing Homework at a Given Time/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int busyStudent(int[] startTime, int[] endTime, int queryTime) {
+        int length = startTime.length;
+        int result = 0;
+
+        for(int i = 0; i < length; i++){
+            if(startTime[i] <= queryTime && queryTime <= endTime[i]){
+                result++;
+            }
+        }
+
+        return result;
+    }
+}
