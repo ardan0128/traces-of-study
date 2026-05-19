08e07d6df2f8c6ecb68f9704de798ac541e69ad6
JWon <jwon1818@daum.net>
2026-05-19 18:06:36 +0900

    LeetCode 1512. Number of Good Pairs

---
+++ algorithm/LeetCode/1512. Number of Good Pairs/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        Map<Integer, Integer> pairs = new HashMap<>();
+        int result = 0;
+
+        for(int i = 0; i < nums.length; i++){
+            if(pairs.containsKey(nums[i])){
+                result += pairs.get(nums[i]);
+            }
+
+            pairs.put(nums[i], pairs.getOrDefault(nums[i], 0) + 1);
+        }
+
+        return result;
+    }
+}
