c325a4a8f9c136eb0bbab7be3290c1e538783008
JWon <jwon1818@daum.net>
2026-06-12 19:00:23 +0900

    LeetCode 1672. Richest Customer Wealth

---
+++ algorithm/LeetCode/1672. Richest Customer Wealth/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int max = 0;
+
+        for(int[] account : accounts){
+            int sum = 0;
+            
+            for(int i = 0; i < account.length; i++){
+                sum += account[i];
+            }
+
+            max = Math.max(max, sum);
+        }
+
+        return max;
+    }
+}
