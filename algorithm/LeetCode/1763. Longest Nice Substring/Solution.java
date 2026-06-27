14a572ca1faa11b660e6ff3a27a86da3176adf43
JWon <jwon1818@daum.net>
2026-06-27 16:39:33 +0900

    LeetCode 1763. Longest Nice Substring

---
+++ algorithm/LeetCode/1763. Longest Nice Substring/Solution.java
@@ -0,0 +1,37 @@
+class Solution {
+    public String longestNiceSubstring(String s) {
+        String result = "";
+
+        for(int i = 0; i < s.length(); i++){
+            for(int j = i; j < s.length(); j++){
+                String sub = s.substring(i, j + 1);
+
+                if(isNice(sub) == true){
+                    if(sub.length() > result.length()){
+                        result = sub;
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public boolean isNice(String s){
+        HashSet<Character> set = new HashSet<>();
+
+        for(char c : s.toCharArray()){
+            set.add(c);
+        }
+
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+
+            if(!(set.contains(Character.toUpperCase(c)) && set.contains(Character.toLowerCase(c)))){
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
