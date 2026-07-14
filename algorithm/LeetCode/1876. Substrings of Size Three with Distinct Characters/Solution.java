ec737166334d57999625de876372c26c167c0193
JWon <jwon1818@daum.net>
2026-07-14 22:33:19 +0900

    LeetCode 1876. Substrings of Size Three with Distinct Characters

---
+++ algorithm/LeetCode/1876. Substrings of Size Three with Distinct Characters/Solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int countGoodSubstrings(String s) {
+        if(s.length() <= 2){
+            return 0;
+        }
+
+        int result = 0;
+
+        for(int i = 0; i < s.length() - 2; i++){
+            if(
+                s.charAt(i) != s.charAt(i + 1) &&
+                s.charAt(i + 1) != s.charAt(i + 2) &&
+                s.charAt(i + 2) != s.charAt(i)
+            ){
+                result++;
+            }
+        }
+
+        return result;
+    }
+}
