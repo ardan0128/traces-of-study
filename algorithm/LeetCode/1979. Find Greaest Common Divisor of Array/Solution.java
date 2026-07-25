e253391ba495bf0ed7ee017c0dca00bf67b2c834
JWon <jwon1818@daum.net>
2026-07-25 21:13:16 +0900

    LeetCode 1979. Find Greatest Common Divisor of Array

---
+++ algorithm/LeetCode/1979. Find Greaest Common Divisor of Array/Solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int findGCD(int[] nums) {
+        int min = nums[0];
+        int max = nums[0];
+
+        for(int num : nums){
+            min = Math.min(min, num);
+            max = Math.max(max, num);
+        }
+
+        return gcd(min, max);
+    }
+
+    private int gcd(int min, int max){
+        while(max != 0){
+            int temp = max;
+            max = min % max;
+            min = temp;
+        }
+
+        return min;
+    }
+}
