e1cf95a910884f0818884ba2542d2b1e9f188391
JWon <jwon1818@daum.net>
2026-07-25 21:03:45 +0900

    LeetCode 1978. Employees Whose manager Left the Company

---
+++ algorithm/LeetCode/1978. Employees Whose Manager Left the Company/Solution.sql
@@ -0,0 +1,6 @@
+-- Write your PostgreSQL query statement below
+select A.employee_id
+from Employees A
+left outer join Employees B on A.manager_id = B.employee_id
+where A.salary < 30000 and A.manager_id is not null and B.employee_id is null
+order by A.employee_id
