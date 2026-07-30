593edda7b171f126214b33b02c1f27c7a253a175
JWon <jwon1818@daum.net>
2026-07-30 11:35:58 +0900

    LeetCode 2027. Minimum Moves to Convert String

---
+++ algorithm/LeetCode/2027. Minimum Move to Convert String/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minimumMoves(String s) {
+        int move = 0;
+
+        for(int i = 0; i < s.length();){
+            if(s.charAt(i) == 'X'){
+                move++;
+                i += 3;
+            }else{
+                i++;
+            }
+        }
+
+        return move;
+    }
+}
