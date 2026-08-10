af6e49359cead96deebbb9eec28d20a9a9da0650
JWon <jwon1818@daum.net>
2026-08-10 14:00:40 +0900

    LeetCode 2138. Divide a String Into Groups of Size k

---
+++ algorithm/LeetCode/2138. Divide a String Into Groups of Size k/Solution.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String[] divideString(String s, int k, char fill) {
+        int n = s.length();
+        int groups = (n + k - 1) / k;
+        String[] result = new String[groups];
+
+        for(int i = 0; i < groups; i++){
+            StringBuilder sb = new StringBuilder();
+
+            for(int j = 0; j < k; j++){
+                int index = i * k + j;
+
+                if(index < n){
+                    sb.append(s.charAt(index));
+                }else{
+                    sb.append(fill);
+                }
+            }
+
+            result[i] = sb.toString();
+        }
+
+        return result;
+    }
+}
