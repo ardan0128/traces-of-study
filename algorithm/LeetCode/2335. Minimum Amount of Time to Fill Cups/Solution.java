93241f8448d25db322ad31047ffb2ef3cabb0023
JWon <jwon1818@daum.net>
2026-08-30 09:23:59 +0900

    LeetCode 2335. Minimum Amount of Time to Fill Cups

---
+++ algorithm/LeetCode/2335. Minimum Amount of Time to Fill Cups/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int fillCups(int[] amount) {
+        int max = 0;
+        int sum = 0;
+
+        for(int a : amount){
+            max = Math.max(a, max);
+            sum += a;
+        }
+
+        return Math.max(max, (sum + 1) / 2);
+    }
+}
