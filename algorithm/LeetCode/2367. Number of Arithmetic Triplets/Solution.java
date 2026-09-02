8bf9bc8d11de6cb7c9314d77a42256fde0e91d4e
JWon <jwon1818@daum.net>
2026-09-02 09:23:10 +0900

    LeetCode 2367. Number of Arithmetic Triplets

---
+++ algorithm/LeetCode/2367. Number of Arithmetic Triplets/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int arithmeticTriplets(int[] nums, int diff) {
+        Set<Integer> set = new HashSet<>();
+
+        for(int num : nums){
+            set.add(num);
+        }
+
+        int count = 0;
+
+        for(int num : nums){
+            if(set.contains(num - diff) && set.contains(num + diff)){
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
