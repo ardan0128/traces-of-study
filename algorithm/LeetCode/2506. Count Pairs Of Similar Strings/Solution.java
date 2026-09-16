4a067b6192e52fd2226ead580b27c58170a31ac9
JWon <jwon1818@daum.net>
2026-09-16 11:51:18 +0900

    LeetCode 2506. Count Pairs Of Similar Strings

---
+++ algorithm/LeetCode/2506. Count Pairs Of Similar Strings/Solution.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int similarPairs(String[] words) {
+        int count = 0;
+
+        for(int i = 0; i < words.length - 1; i++){
+            for(int j = i + 1; j < words.length; j++){
+                count += charCount(words[i], words[j]);
+            }
+        }
+
+        return count;
+    }
+
+    private int charCount(String s1, String s2){
+        Set<Character> set1 = new HashSet<>();
+
+        for(char c : s1.toCharArray()){
+            set1.add(c);
+        }
+
+        Set<Character> set2 = new HashSet<>();
+
+        for(char c : s2.toCharArray()){
+            set2.add(c);
+        }
+
+        return set1.equals(set2) ? 1 : 0;
+    }
+}
